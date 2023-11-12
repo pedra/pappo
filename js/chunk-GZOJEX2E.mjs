4ccc2312d492342ff0c740d0cac3474fe3e019ef
Bill Rocha <ahcordesign@gmail.com>
2023-11-12 10:58:06 -0300

    upgrade

---
+++ js/chunk-GZOJEX2E.mjs
@@ -0,0 +1,2 @@
+import{x as a,z as o}from"./chunk-PLLBGEHY.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-GZOJEX2E.mjs.map
